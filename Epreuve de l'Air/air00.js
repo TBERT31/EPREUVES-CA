dfc2154d74591cf584cb292c127e547a4930708c
TBERT31 <78256617+TBERT31@users.noreply.github.com>
2023-03-07 09:00:29 +0100

    air00.js
    air00.js

---
+++ Epreuve de l'Air/air00.js
@@ -0,0 +1,12 @@
+if(process.argv.length !== 4 ){
+    console.log("Two argument please, a string to Cut + a separator");
+    return;
+}
+
+let stringToCut = process.argv[3];
+
+let separator = process.argv[4];
+
+function cutString(strungToCut, separator){
+    
+}
