d18d79ebcfe1f65dd579969fae11fe3ed40c5416
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-06-07 18:01:07 +0200

    before delete from my pc

--- Epreuve de l'Air/air03.js
+++ Epreuve de l'Air/air03.js
@@ -6,13 +6,28 @@ if(process.argv.length < 3 ){
 
 let arrayToCheck = [];
 
-for(let i = 2; i < process.argv.length-1; i++){
+for(let i = 2; i < process.argv.length; i++){
   arrayToCheck.push(process.argv[i]);
 }
 
 // Parsing
+function findDuplicates(arr){
+  let duplicateArr = [];
+  let fullArr = [];
+  for(let i = 0; i < arr.length; i++){
+      if(fullArr.indexOf(arr[i]) === -1){
+        fullArr.push(arr[i]);
+      }else if(fullArr.indexOf(arr[i]) !== -1){
+        fullArr.push(arr[i]);
+        duplicateArr.push(arr[i]);
+      }
+  }
+  return duplicateArr;
+}
+
+
 function removeDuplicates(arr) {
-  return arr.filter((value, index) => arr.indexOf(value) !== index);
+  return arr.filter(value => !findDuplicates(arr).includes(value));
 }
 
 
